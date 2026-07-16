c4b7c6fbb5c35ce63a5d4394717b59a31fafbb54
ManyaBansal <manyabansal1302@gmail.com>
2026-07-16 18:41:34 +0530

    home-page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,54 @@
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login.jsx";
+import SignUp from "../components/authentication/SignUp.jsx";
+
 const HomePage = () => {
-  return <div>HOME</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="2xl" fontFamily="Work Sans" color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="blue">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
