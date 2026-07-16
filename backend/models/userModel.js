1029b6f34b887a5505c063c4efc7c9cea7958c83
ManyaBansal <manyabansal1302@gmail.com>
2026-07-16 18:42:38 +0530

    user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userModel = monggose.Schema(
+const userModel = mongoose.Schema(
   {
     //name
     //email
@@ -20,6 +21,18 @@ const userModel = monggose.Schema(
     timestamps: true,
   },
 );
+
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userModel);
 module.exports = User;
