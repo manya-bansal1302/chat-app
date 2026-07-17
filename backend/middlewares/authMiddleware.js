61d165eee0f7a308c55cabaf1493c3ca6d0de324
ManyaBansal <manyabansal1302@gmail.com>
2026-07-17 12:50:11 +0530

    middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel.js");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer") //Bearer is a type of token that is used in the authorization header to authenticate requests. It indicates that the client is sending a bearer token for authentication.
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //Bearer token verification
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = { protect };
