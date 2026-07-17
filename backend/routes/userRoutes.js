6e166171acc509882153f3beeb952fb0034c0e38
ManyaBansal <manyabansal1302@gmail.com>
2026-07-17 12:49:44 +0530

    user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const { protect } = require("../middlewares/authMiddleware.js");
 const router = express.Router();
-const { registerUser, authUser } = require("../controllers/userControllers.js");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers.js");
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.route("/").get(protect, allUsers);
 
 module.exports = router;
