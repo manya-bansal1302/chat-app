9c769d83608026e20732a1f0623e3751ffa41190
ManyaBansal <manyabansal1302@gmail.com>
2026-07-17 12:49:58 +0530

    db

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const chats = require("./data/data.js");
 const connectDB = require("./config/db.js");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes.js");
+const chatRoutes = require("./routes/chatRoutes.js");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware.js");
 
 const dotenv = require("dotenv");
@@ -16,18 +17,8 @@ app.get("/", (req, res) => {
   res.send("API is running... successfully");
 });
 
-// app.get("/api/chat", (req, res) => {
-//   res.send(chats);
-//   // console.log(chats);
-// });
-
-// app.get("/api/chat/:id", (req, res) => {
-//   // console.log(req.params.id);
-//   const singlechat = chats.find((c) => c._id === req.params.id);
-//   res.send(singlechat);
-// });
-
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
